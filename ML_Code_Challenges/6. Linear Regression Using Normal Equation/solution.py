631ba17eda1e8f52fa943990dc11443d9f4e2bad
EMikheeva <90220808+EMiheeva@users.noreply.github.com>
2024-07-16 15:25:20 +0200

    Update solution.py

--- ML_Code_Challenges/6. Linear Regression Using Normal Equation/solution.py
+++ ML_Code_Challenges/6. Linear Regression Using Normal Equation/solution.py
@@ -6,9 +6,16 @@ Round your answer to four decimal places, -0.0 is a valid result for rounding a 
 '''
 import numpy as np
 def linear_regression_normal_equation(X: list[list[float]], y: list[float]) -> list[float]:
-    X = np.array(X)
-	  X_transpose = X.T
-    y = np.array(y).reshape(-1, 1)
-	  theta = np.linalg.inv(X_transpose.dot(X)).dot(X_transpose).dot(y)
-    theta = np.round(theta, 4).flatten().tolist()
-    return theta
+	X = np.array(X)
+	X_tr = X.T
+	y = np.array(y).reshape(-1, 1)
+	theta = np.linalg.inv(X_tr.dot(X)).dot(X_tr).dot(y) # вычисление обратной матрицы, как в формуле
+	theta = np.round(theta, 4).flatten() # используется для преобразования массива в одномерный вектор
+	if len(theta) == 2:
+		print(f"The linear model is y = {thate[0] + {theta[1]}*x, perfectly fitteng the input data.")
+	else:
+		model = f"The linear model is y = {theta[0]}"
+		for i in range(1, len(theta)):
+			model += f" + {theta[i]}*x_{i}"
+		print(model)
+	return theta.tolist()
