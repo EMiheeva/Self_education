0a3e5b3f692f16e31b1a573377da041702d55cc8
EMikheeva <90220808+EMiheeva@users.noreply.github.com>
2024-07-16 14:47:16 +0200

    Update solution.py

--- ML_Code_Challenges/1. Matrix times Vector/solution.py
+++ ML_Code_Challenges/1. Matrix times Vector/solution.py
@@ -1,7 +1,12 @@
 '''
 Write a Python function that takes the dot product of a matrix and a vector. return -1 if the matrix could not be dotted with the vector
 '''
+# n - rows
+# m - column
+# len(a[0]) = m
+# len(b) = m
 
+#Solution 1
 def matrix_dot_vector(a:list[list[int|float]],b:list[int|float])-> list[int|float]:
     if len(a[0]) != len(b):
         return -1
@@ -9,7 +14,17 @@ def matrix_dot_vector(a:list[list[int|float]],b:list[int|float])-> list[int|floa
     for i in a:
         element_product_matrix = 0
         for j in range(len(i)):
-            element_product_matrix+=(i[j] * b[j])
+            element_product_matrix += (i[j] * b[j])
         result.append(element_product_matrix)
-
     return result
+
+#Solution 2
+import numpy as np
+def matrix_dot_vector(a:list[list[int|float]],b:list[int|float])-> list[int|float]:
+    if len(a[0]) != len(b):
+        return -1
+    # преобразуем a и b в массивы numpy
+    a = np.array(a)
+    b = np.array(b) 
+    result = np.dot(a,b) #встроенный метод умножения матрицы на вектор
+    return result.tolist() #преобразуем к нужному типу данных
