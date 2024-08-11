84333e63bb26065210b5bcaa5c504523e9bc7e8f
EMikheeva <90220808+EMiheeva@users.noreply.github.com>
2024-08-11 12:40:50 +0200

    Create solution.py

---
+++ ML_Code_Challenges/8. Calculate 2x2 Matrix Inverse/solution.py
@@ -0,0 +1,10 @@
+'''
+Write a Python function that calculates the inverse of a 2x2 matrix. Return 'None' if the matrix is not invertible.
+'''
+def inverse_2x2(matrix: list[list[float]]) -> list[list[float]]:
+    a, b, c, d = matrix[0][0], matrix[0][1], matrix[1][0], matrix[1][1]
+    determinant = a * d - b * c
+    if determinant == 0:
+        return None
+    inverse = [[d/determinant, -b/determinant], [-c/determinant, a/determinant]]
+    return inverse
