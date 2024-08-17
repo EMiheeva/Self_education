a8bdeda6d3c278bb134f9a5b2a075e825b446d9a
EMikheeva <90220808+EMiheeva@users.noreply.github.com>
2024-08-17 10:39:11 +0200

    Create solution.py

---
+++ ML_Code_Challenges/32. Generate Polynomial Features/solution.py
@@ -0,0 +1,24 @@
+'''
+Write a Python function to generate polynomial features for a given dataset. 
+The function should take in a 2D numpy array X and an integer degree, and return a new 2D numpy array with polynomial features up to the specified degree.
+'''
+import numpy as np
+from itertools import combinations_with_replacement
+
+def polynomial_features(X, degree):
+    n_samples, n_features = np.shape(X)
+
+    def index_combinations():
+        combs = [combinations_with_replacement(range(n_features), i) for i in range(0, degree + 1)]
+        flat_combs = [item for sublist in combs for item in sublist]
+        return flat_combs
+    
+    combinations = index_combinations()
+    n_output_features = len(combinations)
+    X_new = np.empty((n_samples, n_output_features))
+    
+    for i, index_combs in enumerate(combinations):  
+        X_new[:, i] = np.prod(X[:, index_combs], axis=1)
+
+    return X_new
+    
