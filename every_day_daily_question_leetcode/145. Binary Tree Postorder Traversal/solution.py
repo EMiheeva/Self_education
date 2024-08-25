414a0389fca97ebc31c9a14f95aec450f6ba73cf
EMikheeva <90220808+EMiheeva@users.noreply.github.com>
2024-08-25 12:00:31 +0200

    Create solution.py

---
+++ every_day_daily_question_leetcode/145. Binary Tree Postorder Traversal/solution.py
@@ -0,0 +1,24 @@
+'''
+TO DO: Given the root of a binary tree, return the postorder traversal of its nodes' values.
+
+Example 1:
+Input: root = [1,null,2,3]
+Output: [3,2,1]
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+        def postorder(root):
+            if not root:
+                return 
+            postorder(root.left)
+            postorder(root.right)
+            result.append(root.val)
+        postorder(root)
+        return result
